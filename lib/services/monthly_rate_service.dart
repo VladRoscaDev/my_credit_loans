6136d27a97ce3aab5a4a9d09fbfb14104e019dea
Vlad Rosca <vlad.rosca@stsnet.ro>
2021-07-29 15:43:44 +0300

    update comments

--- lib/services/monthly_rate_service.dart
+++ lib/services/monthly_rate_service.dart
@@ -3,7 +3,7 @@ import 'package:my_credit_loans/enum/time_period.dart';
 class MonthlyRateService {
   ///method that computes monthly rate
   ///[moneyAmount] the amount of money to borrow
-  ///[numberOfMonths] the number of months for the loan
+  ///[timePeriod] the value from the enum according to the loan
   static double computeMonthlyRate(int moneyAmount, TimePeriod timePeriod) {
     return moneyAmount / timePeriod.numberOfMonths * 1 / 100;
   }
